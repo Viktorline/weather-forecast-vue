d29d977633098db650afcb7edcbe4c89c4cbfe91
Victor Murzin <viktorlinework@gmail.com>
2023-05-12 04:59:45 +0700

    fix api exposing

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const API_KEY = '71566f72957a2e819fa530feb93cdc6c'
+export const API_KEY = import.meta.env.VITE_API_KEY
 export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
